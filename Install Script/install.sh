447ee6f7373c621f3033a42a39e1d660ca51743f
Roschlynn Michael D'souza <53754448+roschlynnmichael@users.noreply.github.com>
2024-05-09 04:06:08 -0500

    Fixed buggy install.sh

--- Install Script/install.sh
+++ Install Script/install.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-#still buggy
+# Error handling
+set -e
 
 echo "Charm Crypto Installation"
 echo "Beginning install of Charm Crypto"
-mkdir packages
+
+# Create directory for packages
+mkdir -p packages
 cd packages
 
-#Update all packages
+# Update all packages
 sudo apt-get update
 sudo apt-get upgrade -y
 
-#Install GCC Make Perl
-sudo apt-get install gcc make perl -y
+# Install necessary packages
+sudo apt-get install gcc make perl m4 flex bison python3-setuptools python3-dev libssl-dev python3-pip git curl -y
 
-#Install M4 Flex Bison
-sudo apt-get install m4 flex bison -y
+# Check if OpenSSL, GMP, and PBC files exist before downloading
+if [ ! -f "openssl-1.0.0s.tar.gz" ]; then
+    wget https://www.openssl.org/source/old/1.0.0/openssl-1.0.0s.tar.gz
+fi
 
-#Install Python Dependency Packages
-sudo apt-get install python3-setuptools python3-dev libssl-dev python3-pip -y
+if [ ! -f "gmp-5.1.3.tar.bz2" ]; then
+    wget https://gmplib.org/download/gmp/gmp-5.1.3.tar.bz2
+fi
 
-#Install PyParsing
-pip3 install pyparsing==2.4.6
+if [ ! -f "pbc-0.5.14.tar.gz" ]; then
+    wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
+fi
 
-#Compile and Install OpenSSL
-wget https://www.openssl.org/source/old/1.0.0/openssl-1.0.0s.tar.gz
+# Extract files
 sudo tar -xzvf openssl-1.0.0s.tar.gz -C /usr/local/src
+sudo tar -jxvf gmp-5.1.3.tar.bz2 -C /usr/local/src
+sudo tar -zxvf pbc-0.5.14.tar.gz -C /usr/local/src/
+
+# Compile and Install OpenSSL
 cd /usr/local/src/openssl-1.0.0s/
 sudo ./config shared --prefix=/usr/local/openssl --openssldir=/usr/lib/openssl
 sudo make
@@ -35,38 +45,26 @@ sudo ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
 sudo ln -s /usr/local/openssl/include/openssl /usr/include/openssl
 sudo ln -s /usr/local/openssl/lib/libssl.so.1.0.0 /usr/lib/libssl.so
 sudo ln -s /usr/local/openssl/lib/libcrypto.so.1.0.0 /usr/lib/libcrypto.so
-echo "Test OpenSSL Version Now!"
+
+# Test OpenSSL version
+echo "Testing OpenSSL Version Now!"
 sleep 3
 openssl version
 sleep 3
 
-cd ~
-cd packages
-
-#Compile and Install GMP
-wget https://gmplib.org/download/gmp/gmp-5.1.3.tar.bz2
-sudo tar -jxvf gmp_5.1.3.tar.bz2 -C /usr/local/src
+# Compile and Install GMP
 cd /usr/local/src/gmp-5.1.3
 sudo ./configure
 sudo make
 sudo make install
 
-cd ~
-cd packages
-
-#Compile and Install PBC
-wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
-sudo tar -zxvf pbc-0.5.14.tar.gz -C /usr/local/src/
+# Compile and Install PBC
 cd /usr/local/src/pbc-0.5.14/
 sudo ./configure
 sudo make
 sudo make install
 
-cd ~
-cd packages
-
-#Compile and Install Charm Crypto
-sudo apt-get install git curl -y
+# Compile and Install Charm Crypto
 git clone https://github.com/JHUISI/charm.git
 sudo mv ./charm /usr/local/src
 cd /usr/local/src/charm
@@ -74,4 +72,4 @@ sudo ./configure.sh
 sudo make
 sudo make install
 
-#Script only works on Windows Subsystem for Linux V2
+# Script only works on Windows Subsystem for Linux V2
